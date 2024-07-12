eaeeee46a744c0578259d8bd97e4e038f9deaff6
Alexander Levin <alexander.levin@omio.com>
2024-07-12 17:43:50 +0200

    Upgrade telegram bot library, add proper start, privacy and description

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,8 @@
 import dev.inmo.tgbotapi.bot.ktor.telegramBot
 import dev.inmo.tgbotapi.extensions.api.bot.getMe
+import dev.inmo.tgbotapi.extensions.api.bot.setMyCommands
+import dev.inmo.tgbotapi.extensions.api.bot.setMyDescription
+import dev.inmo.tgbotapi.extensions.api.bot.setMyShortDescription
 import dev.inmo.tgbotapi.extensions.api.send.media.sendPhoto
 import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.api.send.sendMessage
@@ -9,12 +12,15 @@ import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPoll
 import dev.inmo.tgbotapi.extensions.behaviour_builder.expectations.waitDataCallbackQuery
 import dev.inmo.tgbotapi.extensions.behaviour_builder.expectations.waitText
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
+import dev.inmo.tgbotapi.extensions.behaviour_builder.utils.extensions.onCommandPrivacy
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.dataButton
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.inlineKeyboard
 import dev.inmo.tgbotapi.requests.abstracts.asMultipartFile
+import dev.inmo.tgbotapi.types.BotCommand
 import dev.inmo.tgbotapi.types.UserId
 import dev.inmo.tgbotapi.types.chat.PreviewChat
+import dev.inmo.tgbotapi.utils.RiskFeature
 import dev.inmo.tgbotapi.utils.row
 import kotlinx.coroutines.flow.first
 import qrcode.QRCode
@@ -23,9 +29,31 @@ data class Account(val alias: String, val iban: String, val name: String)
 
 val cache = mutableMapOf<UserId, MutableSet<Account>>()
 
-suspend fun main() {
+val startText = """
+Hi! I'm a SEPA QR code generator bot.
+To generate a QR code, use the /generate command.
+You can simplify QR code generation by adding accounts with /add command.
+You can remove accounts with /remove or /remove_all commands.
+To learn what data this bot stores, use the /privacy command.
+                
+Not sure what SEPA QR code is? Check out https://en.wikipedia.org/wiki/EPC_QR_code
+""".trimIndent()
 
-    val bot = telegramBot(System.getenv("TELEGRAM_BOT_TOKEN"))
+val privacyText = """
+This bot only stores account information you are explicitly provided with /add command.
+The purpose of this information is only to simplify SEPA QR code generation.
+You can remove accounts with /remove or /remove_all commands.
+The bot does not store any information provided for /generate command.
+""".trimIndent()
+
+const val description = "Bot that can generate SEPA QR code (formally EPC QR Code) for you"
+
+@OptIn(RiskFeature::class)
+suspend fun main() {
+    val telegramToken = requireNotNull(System.getenv("TELEGRAM_BOT_TOKEN")) {
+        "TELEGRAM_BOT_TOKEN environment variable is not specified"
+    }
+    val bot = telegramBot(telegramToken)
 
     bot.buildBehaviourWithLongPolling {
         println(getMe())
@@ -68,6 +96,13 @@ suspend fun main() {
             println("remove command for ${it.from} finished")
         }
 
+        onCommand("remove_all") {
+            println("remove_all command for ${it.from} started")
+            cache.remove(it.from!!.id)
+            reply(it, "All accounts removed")
+            println("remove_all command for ${it.from} finished")
+        }
+
         onCommand("generate") {
             println("generate command for ${it.from} started")
             val accounts = cache[it.from!!.id] ?: emptySet()
@@ -104,8 +139,24 @@ suspend fun main() {
         }
 
         onCommand("start") {
-            reply(it, "Hi:)")
+            reply(it, startText)
         }
+
+        onCommandPrivacy(privacyText)
+
+        setMyCommands(
+            listOf(
+                BotCommand("generate", "Generate SEPA QR code"),
+                BotCommand("add", "Add account info to use in the future"),
+                BotCommand("remove", "Remove account from the list of saved accounts"),
+                BotCommand("remove_all", "Remove all accounts from the list of saved accounts"),
+                BotCommand("start", "Get introduction for this bot"),
+                BotCommand("privacy", "Learn what data this bot stores and why")
+            )
+        )
+
+        setMyDescription(description)
+        setMyShortDescription(description)
     }.join()
 }
 
